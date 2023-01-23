ce7966f4bfd2aab84caf364a1a5427b325637321
Simachew Mengiste <absima@gmail.com>
2023-01-23 19:20:23 +0100

    no biggie, but a tip to add new contacts

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,6 @@
 export default function Index() {
   return (
     <p id="zero-state">
-      <span>There are no contacts yet.</span>
       <span>Click the "New" button to add one.</span>
     </p>
   );
